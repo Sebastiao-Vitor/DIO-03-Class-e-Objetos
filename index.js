efe49f924407328543104f8f4eef04d7d1c2890f
Sebastiao-Vitor <sebastiaovitor18@gmail.com>
2024-10-18 20:54:03 -0300

    Atualização das classes

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-// class generica
-class chanpions{
-  constructor(name, age, []) {
+// // class generica
+// class chanpions{
+//   constructor(name, age, []) {
+//     this.chanpionsName = name;
+//     this.chanpionsAge = age;
+//     this.chanpionsTipo = ["guerreiro", "mago"];
+//   };
+//   personagemClass() {
+//     let mensagem = "";
+//     switch(this.chanpionsTipo) {
+//       case "guerreiro":
+//         mensagem = "espada";
+//         break;
+//       case "mago":
+//         mensagem = "magia";
+//         break;
+//       case "monge":
+//         mensagem = "artes marciais";
+//         break;
+//       case "ninja":
+//         mensagem = "shuriken";
+//         break;
+//       default:
+//         mensagem = "!!tipo desconhecido!!";
+//     };
+//     return mensagem;
+//   };
+//   atacar() {
+//     console.log(`O ${this.chanpionsTipo} atacou usando ${this.personagemClass()}`);
+//   };
+// };
+
+// let chanpionsGuerreiro = new chanpions("Denvor", 35, "guerreiro");
+// chanpionsGuerreiro.atacar();
+
+class Chanpions {
+  constructor(name, age, tipoIndex) {
     this.chanpionsName = name;
     this.chanpionsAge = age;
-    this.chanpionsTipo = ["guerreiro", "mago"];
+    
+    // Lista de tipos de campeões
+    this.tiposDisponiveis = ["guerreiro", "mago", "monge", "ninja"];
+    
+    // Mapear o índice para o tipo correspondente
+    this.chanpionsTipo = this.tiposDisponiveis[tipoIndex];
   };
-  personagemClass() {
+  classeEscolida(){
     let mensagem = "";
     switch(this.chanpionsTipo) {
       case "guerreiro":
@@ -23,12 +62,12 @@ class chanpions{
       default:
         mensagem = "!!tipo desconhecido!!";
     };
-    return mensagem;
-  };
-  atacar() {
-    console.log(`O ${this.chanpionsTipo} atacou usando ${this.personagemClass()}`);
+  }
+  ataque() {
+    console.log(`Voçe escolheu o personagem: ${this.chanpionsTipo}, agora voçê pode usar os poderes do ${this.chanpionsTipo}`);
   };
 };
 
-let chanpionsGuerreiro = new chanpions("Denvor", 35, "guerreiro");
-chanpionsGuerreiro.atacar();
+let MMORPG = new Chanpions("Carton", 28, 1);
+console.log(MMORPG);
+MMORPG.ataque()
